7caf26ef32cfa84ef8888965dfa15e703bde9dff
Petr Tcoi <petrtcoi@outlook.com>
2022-12-20 14:50:58 +0300

    add https

--- dist/index.js
+++ dist/index.js
@@ -5,15 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.wss = void 0;
 const app_1 = __importDefault(require("./app"));
+const https_1 = __importDefault(require("https"));
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const url_1 = require("url");
 const websocket_1 = __importDefault(require("./features/auction/websocket"));
 const port = process.env.PORT;
 const mode = process.env.MODE;
-const server = app_1.default.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}. Mode: ${mode}`);
+const httpServer = http_1.default.createServer(app_1.default)
+    .listen(80, () => {
+    console.log(`⚡️HTTP[server]: Server is running at https://localhost:80. Mode: ${mode}`);
 });
-exports.wss = (0, websocket_1.default)(server);
-server.on('upgrade', function upgrade(request, socket, head) {
+const httpsServer = https_1.default.createServer({
+    key: fs_1.default.readFileSync(path_1.default.resolve('./src/config/key.pem')),
+    cert: fs_1.default.readFileSync(path_1.default.resolve('./src/config/cert.pem')),
+    passphrase: 'helloworld'
+}, app_1.default)
+    .listen(443, () => {
+    console.log(`⚡️HTTPS[server]: Server is running at https://localhost:443. Mode: ${mode}`);
+});
+exports.wss = (0, websocket_1.default)(httpsServer);
+httpsServer.on('upgrade', function upgrade(request, socket, head) {
     const { pathname } = (0, url_1.parse)((request === null || request === void 0 ? void 0 : request.url) || '');
     if (pathname === '/auction') {
         exports.wss.handleUpgrade(request, socket, head, function done(ws) {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import app from './app'
+import https from 'https'
+import http from 'http'
+import fs from 'fs'
+import path from 'path'
 import { Server, WebSocket } from 'ws'
 import { parse } from 'url'
 
@@ -8,13 +12,23 @@ import wssAuction from './features/auction/websocket'
 const port = process.env.PORT
 const mode = process.env.MODE
 
-const server = app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}. Mode: ${mode}`)
-})
 
-export const wss = wssAuction(server as unknown as Server<WebSocket>)
+const httpServer = http.createServer(app)
+  .listen(80, () => {
+    console.log(`⚡️HTTP[server]: Server is running at https://localhost:80. Mode: ${mode}`)
+  })
+const httpsServer = https.createServer({
+  key: fs.readFileSync(path.resolve('./src/config/key.pem')),
+  cert: fs.readFileSync(path.resolve('./src/config/cert.pem')),
+  passphrase: 'helloworld'
+}, app)
+  .listen(443, () => {
+    console.log(`⚡️HTTPS[server]: Server is running at https://localhost:443. Mode: ${mode}`)
+  })
 
-server.on('upgrade', function upgrade(request, socket, head) {
+export const wss = wssAuction(httpsServer as unknown as Server<WebSocket>)
+
+httpsServer.on('upgrade', function upgrade(request, socket, head) {
   const { pathname } = parse(request?.url || '')
   if (pathname === '/auction') {
     wss.handleUpgrade(request, socket, head, function done(ws) {
